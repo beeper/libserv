6d55885ea4b70689921c4fe31c206631cc7a4371
Sumner Evans <sumner@beeper.com>
2023-05-23 16:08:55 -0600

    requestlog: add Route to track HTTP response times
    Signed-off-by: Sumner Evans <sumner@beeper.com>

---
+++ pkg/requestlog/route.go
@@ -0,0 +1,56 @@
+package requestlog
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"strings"
+)
+
+type Route struct {
+	Path    string
+	Method  string
+	Handler http.HandlerFunc
+
+	TrackHTTPResponseTime func(string) func(int)
+	MetricsEndpoint       string
+
+	LogContent bool
+}
+
+var _ http.Handler = (*Route)(nil)
+
+func (rt *Route) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	crw := w.(*CountingResponseWriter)
+	if rt.TrackHTTPResponseTime != nil {
+		defer rt.TrackHTTPResponseTime(rt.MetricsEndpoint)(crw.StatusCode)
+	}
+	if rt.LogContent {
+		if r.Method != http.MethodGet && r.Method != http.MethodHead {
+			crw.ResponseBody = &bytes.Buffer{}
+		}
+		if strings.HasPrefix(r.Header.Get("Content-Type"), "application/json") {
+			pcr := &partialCachingReader{Reader: r.Body}
+			crw.RequestBody = &pcr.Buffer
+			r.Body = pcr
+		}
+	}
+	rt.Handler(w, r)
+}
+
+type partialCachingReader struct {
+	Reader io.ReadCloser
+	Buffer bytes.Buffer
+}
+
+func (pcr *partialCachingReader) Read(p []byte) (int, error) {
+	n, err := pcr.Reader.Read(p)
+	if n > 0 {
+		pcr.Buffer.Write(CutRequestData(p[:n], pcr.Buffer.Len()))
+	}
+	return n, err
+}
+
+func (pcr *partialCachingReader) Close() error {
+	return pcr.Reader.Close()
+}
