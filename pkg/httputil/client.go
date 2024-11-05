aee2b3b7fbe596bd5b2b04894940945d23a9d28f
Sumner Evans <sumner.evans@automattic.com>
2024-11-05 03:52:38 -0700

    httputil: allow creating new default transports
    Signed-off-by: Sumner Evans <sumner.evans@automattic.com>

--- pkg/httputil/client.go
+++ pkg/httputil/client.go
@@ -8,20 +8,26 @@ import (
 
 const DefaultTimeout = time.Second * 15
 
+// NewDefaultTransport returns a new default [http.Transport] with a 15 second
+// dial and TLS handshake timeout and HTTP/2 support disabled.
+func NewDefaultTransport() *http.Transport {
+	return &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		Dial: (&net.Dialer{
+			Timeout:   DefaultTimeout,
+			KeepAlive: 30 * time.Second,
+		}).Dial,
+		ForceAttemptHTTP2:     false, // never use HTTP/2
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   DefaultTimeout,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
+}
+
 // DeafultTransport is a http.Transport with a 15 second dial and TLS handshake
 // timeout and HTTP/2 support disabled.
-var DefaultTransport = &http.Transport{
-	Proxy: http.ProxyFromEnvironment,
-	Dial: (&net.Dialer{
-		Timeout:   DefaultTimeout,
-		KeepAlive: 30 * time.Second,
-	}).Dial,
-	ForceAttemptHTTP2:     false, // never use HTTP/2
-	MaxIdleConns:          100,
-	IdleConnTimeout:       90 * time.Second,
-	TLSHandshakeTimeout:   DefaultTimeout,
-	ExpectContinueTimeout: 1 * time.Second,
-}
+var DefaultTransport = NewDefaultTransport()
 
 // DefaultClient is a http.Client using the DefaultTransport.
 var DefaultClient = &http.Client{
